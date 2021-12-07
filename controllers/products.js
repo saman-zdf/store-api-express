0e4f8977eae20792090de876339d55a1ec350625
saman-zdf <samanfyz68@gmail.com>
2021-12-07 16:33:14 +0800

    query operation for featured, company has been added, for name the query is been added with $regex helper from mongoDB

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,33 @@
+const { Product } = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('Testing async errors');
-  res.status(200).json({ meg: 'Product route' });
+  const search = 'a';
+  const products = await Product.find({
+    name: { $regex: search, $options: 'i' },
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ meg: 'Product testing route' });
+  // we pull out all the query we want to implement from requset.query
+  const { featured, company, name } = req.query;
+  // declare an empty object
+  const queryObject = {};
+  // we check if the query added to the url we assing a new property to the object by the name of featured and will chck if the query is true or false
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  // the same procedure applies to the comapny which is coming from request.query
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 module.exports = {
   getAllProducts,
