edb25a3cb8fd28f50b7f22a1fb281dd417b6d00a
saman-zdf <samanfyz68@gmail.com>
2021-12-07 10:53:40 +0800

    product model has been added

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,10 @@
-const getAllProducts = async (req, res) => {
-  res.status(200).json({ meg: 'Product testing route' });
+const getAllProductsStatic = async (req, res) => {
+  throw new Error('Testing async errors');
+  res.status(200).json({ meg: 'Product route' });
 };
 
-const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ meg: 'Product route' });
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ meg: 'Product testing route' });
 };
 module.exports = {
   getAllProducts,
---
+++ models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Product name must be provided'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Product price must be provided'],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    company: {
+      type: String,
+      enum: {
+        values: ['ikea', 'liddy', 'caressa', 'marcos'],
+        message: '{VALUE} is not supported',
+      },
+      // enum: ["ikea", 'liddy', 'caressa', 'marcos'],
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = { Product };
